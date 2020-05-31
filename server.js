026009f559be6822904442fc7b62ecae24a7819b
AmanGupta03 <theamangupta27@gmail.com>
2020-06-01 01:17:39 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const exec = require("child_process").exec;
 const port =  process.env.PORT || '8000'
 
 app.get('/execute', (req, res) => {
+  
   cmd = req.query.lang + ' ' + req.query.script   //command to run script
+
   exec(cmd, (error, stdout, stderr) => {
-    res.send(stdout)
+    if (stderr) res.send({status:'fail', error:stderr})
+    res.send({status:'success', output:stdout})
   })
 })
 
