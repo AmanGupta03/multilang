bd60c7bbdd4bc15f4dc924df0cbdd5798d4f2955
Pratik Mehta <43946536+Pratikmehta1729@users.noreply.github.com>
2020-06-07 21:21:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const HOST = '0.0.0.0';
 //App
 const app = express();
 
+// ***connection with DB ***
+
 //Middlewares
 app.use(cors());
 app.use(express.json()); // for parsing application/json
@@ -22,11 +24,25 @@ app.use((req, res, next) => {
 });
 
 //Routes
-app.post('/run', async(req, res) => {
+app.post('/dryRun', async(req, res) => {
   const {code,lang,vars} = req.body;
   res.send(await execute(code, lang, vars, req.id));
 });
 
+app.post('/runInFlow',asyns(req, res)=>{
+//   req.body._id will be id for the given code(flow id)
+//    const code = from DB
+//    const lang = from DB
+   const vars = req.body.vars;
+   res.send(await execute(code, lang, vars, req.id));
+});
+app.post('/uploadCode', async(req, res)=>{
+  const code = req.body.code //code in string
+  const lang = req.body.lang //string
+  const flowStepId = req.body._id //flowid + stepid
+//   save(req.body) in mongo
+  
+})
 
 app.listen(PORT, HOST, () => {
   console.log(`Running at http://${HOST}:${PORT}`);
