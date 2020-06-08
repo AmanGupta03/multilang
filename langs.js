423438004d8663c7ca2227005ba39fb9ebaa1f87
AmanGupta03 <theamangupta27@gmail.com>
2020-06-08 12:07:30 +0530

    upgrade addStdout

--- langs.js
+++ langs.js
@@ -9,9 +9,10 @@ exports.langs = {
   python: {
     ext: '.py',
     addStdout : (code) => {
-          let lastToken = code.substring(code.lastIndexOf('\n') + 1).split(';').pop();
-          lastToken = 'print(' + lastToken + ')';
-          return code.substring(0, code.lastIndexOf('\n')) + '\n' + lastToken;
+          let lastLine = code.substring(code.lastIndexOf('\n') + 1).replace(/(;|\s)+$/g, '');
+          const lastToken = 'print(' + lastLine.split(';').pop() + ')';
+          lastLine = lastLine.substring(0, lastLine.lastIndexOf(';')+1) + lastToken; 
+          return code.substring(0, code.lastIndexOf('\n')) + '\n' + lastLine;
       },
     cmd: (dir) => {
           return `docker run --rm  --network=${net} --memory=${mem} --memory-swap=${mem} -v ${dir}:/prog python:3.8-slim-buster python /prog/prog.py`;
@@ -21,9 +22,10 @@ exports.langs = {
   javascript: {
     ext: '.js',
     addStdout : (code) => {
-          let lastToken = code.substring(code.lastIndexOf('\n') + 1).split(';').pop();
-          lastToken = 'console.log(' + lastToken + ')';
-          return code.substring(0, code.lastIndexOf("\n")) + '\n' + lastToken;
+          let lastLine = code.substring(code.lastIndexOf('\n') + 1).replace(/(;|\s)+$/g, '');
+          const lastToken = 'console.log(' + lastLine.split(';').pop() + ')';
+          lastLine = lastLine.substring(0, lastLine.lastIndexOf(';')+1) + lastToken; 
+          return code.substring(0, code.lastIndexOf('\n')) + '\n' + lastLine;
       },
     cmd: (dir) => {
           return `docker run --rm --network=${net} --memory=${mem} --memory-swap=${mem} -v ${dir}:/prog node:14 node /prog/prog.js`;
@@ -34,9 +36,10 @@ exports.langs = {
     ext: '.php',
     addStdout : (code) => {
           code = code.substring(0, code.lastIndexOf('?')).trim();
-          let lastToken = code.substring(code.lastIndexOf('\n') + 1).split(';').pop();
-          lastToken = 'echo(' + lastToken + ')';
-          return code.substring(0, code.lastIndexOf('\n')) + '\n' + lastToken + '\n?>';
+          let lastLine = code.substring(code.lastIndexOf('\n') + 1).replace(/(;|\s)+$/g, '');
+          const lastToken = 'echo(' + lastLine.split(';').pop() + ')';
+          lastLine = lastLine.substring(0, lastLine.lastIndexOf(';')+1) + lastToken; 
+          return code.substring(0, code.lastIndexOf('\n')) + '\n' + lastLine + '\n?>';
       },
     cmd: (dir) => {
           return `docker run --rm --network=${net} --memory=${mem} --memory-swap=${mem} -v ${dir}:/prog php:7.4-cli php /prog/prog.php`;
