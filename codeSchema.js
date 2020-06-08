4828869f119cce965f9756cffaff79df153cd43e
Pratik Mehta <43946536+Pratikmehta1729@users.noreply.github.com>
2020-06-08 12:07:20 +0530

    Update codeSchema.js

--- codeSchema.js
+++ codeSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
-const codeSchema = new mongoose.Schema({id: { type: String, index: true },lang: String,code: String});
+const varObj = new mongoose.Schema({..}, { strict: false });
+const codeSchema = new mongoose.Schema({id: { type: String, index: true },lang: String,code: String,varObj:varObj});
 // compile model from schema
 exports.codeSchema = mongoose.model("file", codeSchema,"file");
